a440e587cb02fd4ecb1976952f902ef8e13f188c
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:27:19 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioPropostoCompsicao01/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ExercicioPropostoCompsicao01.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
